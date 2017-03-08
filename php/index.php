635452b3f230d171980ca5f20a0ca3b96bc685a8
kucik <none@none>
2017-03-08 12:26:16 +0000

    Added trivial php index.

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Digitemp</title>
+</head>
+<body bgcolor=#aaa>
+<h1>Digitemp</h1>
+<iframe width="900" height="800" frameborder="0" scrolling="no" src="//plot.ly/~kucik/10.embed"></iframe>
+</body>
+</html>
